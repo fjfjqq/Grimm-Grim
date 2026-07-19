2bae7e3b0e702370e1ab146aa1766e6a435e856a
fjfjqq <fjfj1234ha@gmail.com>
2026-07-20 02:39:10 +0900

    레이케스트 방식 실패 다른걸로 바꾸기

--- Assets/코드/enemy.cs
+++ Assets/코드/enemy.cs
@@ -5,32 +5,54 @@ public class enemy : MonoBehaviour
 {
     public int maxhp = 3;
     public int nowhp = 3;
-    public int turnpoint1;
-    public int turnpoint2;
-    public float movespeed = 2f;
-        
-    public Vector2 movethewall = Vector2.left;
+    public float movespeed = 2f;     
     public float inground = 0.6f;
+    public LayerMask groundcheck;
 
     private Rigidbody2D rb;
+    public Vector2 movetheground = Vector2.left;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
-        
-
     }
 
     void Update()
     {
-        rb.linearVelocity = movethewall * movespeed;
+        rb.linearVelocity = movetheground * movespeed;
 
-        Vector2 sideDirection;
+        Vector2 movethewall;
 
+        if (movetheground == Vector2.left)
+        {
+            movethewall = Vector2.down;
+        }
+        else if(movetheground == Vector2.down)
+        {
+            movethewall = Vector2.right;
+        }   
+        else if(movetheground == Vector2.right)
+        {
+            movethewall = Vector2.up;
+        }
+        else
+        {
+            movethewall = Vector2.left;
+        }
 
+        RaycastHit2D fronthit = Physics2D.Raycast(transform.position, movetheground, inground, groundcheck);
 
-        
+        RaycastHit2D sidehit = Physics2D.Raycast(transform.position, movethewall, inground, groundcheck);
+
+        if(fronthit.collider != null)
+        {
+            movetheground = -movethewall;
+        }
+        else if (sidehit == null)
+        {
+            movetheground = movethewall;
+        }
+
     }
 
 }
