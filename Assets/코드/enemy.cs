04f1a9dfe0c4f9882688559def1f1cf2b81bfcef
fjfjqq <fjfj1234ha@gmail.com>
2026-07-21 02:01:50 +0900

    레이케스트 방식 실패 다른 방법 고안

--- Assets/코드/enemy.cs
+++ Assets/코드/enemy.cs
@@ -7,13 +7,6 @@ public class enemy : MonoBehaviour
     public int nowhp = 3;
 
     public float movespeed = 2f;
-    public bool turnclock = true;
-
-    public float groundcheck = 0.6f;
-    public float sidecheck = 0.3f;
-    public LayerMask ground;
-
-    public Vector2 down = Vector2.down;
     void Start()
     {
 
@@ -21,74 +14,7 @@ public class enemy : MonoBehaviour
 
     void Update()
     {
-        Vector2 laycast = Nowmoving();
-
-        Debug.Log("laycast" + laycast + "down" + down);
-
-        Debug.DrawRay(transform.position, down * groundcheck, Color.red);
-
-        if (Frontup(laycast))
-        {
-            turncorner();
-            laycast = Nowmoving();
-        }
-
-        transform.position += (Vector3)(laycast * movespeed * Time.deltaTime);
-        shootlaycast();
-        rollthesurface();
-
-
     }
-
-    Vector2 Nowmoving()
-    {
-        if (turnclock)
-        {
-            return new Vector2(down.y, -down.x);
-        }
-        else
-        {
-            return new Vector2(-down.y, down.x);
-        }
-    }
-
-    void turncorner()
-    {
-        if (turnclock)
-        {
-            down = new Vector2(down.y, -down.x);
-        }
-        else
-        {
-            down = new Vector2(-down.y, down.x);
-        }
-    }
-
-    bool Frontup(Vector2 laycast)
-    {
-        Vector2 start = (Vector2)transform.position + laycast * sidecheck;
-
-        RaycastHit2D hit = Physics2D.Raycast(start, down, groundcheck, ground);
-
-        return hit.collider == null;
-    } 
-
-    void shootlaycast()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, down, groundcheck, ground);
-
-        if(hit.collider != null)
-        {
-            float off = 0.1f;
-            transform.position = hit.point - down * off;
-        }
-    }
-
-    void rollthesurface()
-    {
-        transform.rotation = Quaternion.FromToRotation(Vector2.down, down);
-    }
-
 }
 
 
