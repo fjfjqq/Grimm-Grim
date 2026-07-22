e5a47dff7161299cf33fa68ec2180fd0b5e7b4b3
fjfjqq <fjfj1234ha@gmail.com>
2026-07-23 04:46:59 +0900

    표면 감지형식으로 바꿈(버그)
    승천해서 버그남

--- Assets/코드/enemy.cs
+++ Assets/코드/enemy.cs
@@ -7,13 +7,55 @@ public class enemy : MonoBehaviour
     public int nowhp = 3;
 
     public float movespeed = 2f;
+    public bool turnpoint = true;
+
+    public Collider2D lastturn = null;
+
+    public Vector2 down = Vector2.down;
     void Start()
     {
-
+        
     }
 
     void Update()
     {
+        Vector2 enemy = Nowmoving();
+        transform.position += (Vector3)(enemy * movespeed * Time.deltaTime);
+        rollthesurface();
+    }
+
+    Vector2 Nowmoving()
+    {
+        if (turnpoint)
+        {
+            return new Vector2(down.y, -down.x);
+        }
+        {
+            return new Vector2(-down.y, down.x);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("turnpoint") && collision != lastturn)
+        {
+
+            lastturn = collision;
+
+            if (turnpoint)
+            {
+                down = new Vector2(down.y, -down.x);
+            }
+            else
+            {
+                down = new Vector2(-down.y, down.x);
+            }
+        }
+    }
+
+    void rollthesurface()
+    {
+        transform.rotation = Quaternion.FromToRotation(Vector2.down, down);
     }
 }
 
