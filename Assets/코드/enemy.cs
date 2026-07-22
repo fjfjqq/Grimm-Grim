a4664c3c0ec912133889b22a9c3919f13e0c0715
fjfjqq <fjfj1234ha@gmail.com>
2026-07-23 05:23:06 +0900

    따라가게 하는 형식으로 완성
    포인트 지정후 그 위치에 도달시 회전하게 한후
    다음 목표로 이동하게 수정

--- Assets/코드/enemy.cs
+++ Assets/코드/enemy.cs
@@ -1,62 +1,42 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class enemy : MonoBehaviour 
+public class enemy : MonoBehaviour
 {
     public int maxhp = 3;
     public int nowhp = 3;
 
     public float movespeed = 2f;
-    public bool turnpoint = true;
+    public Transform[] turnpoint;
 
-    public Collider2D lastturn = null;
-
-    public Vector2 down = Vector2.down;
+    public int nowpoint = 0;
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        Vector2 enemy = Nowmoving();
-        transform.position += (Vector3)(enemy * movespeed * Time.deltaTime);
-        rollthesurface();
-    }
+        Transform target = turnpoint[nowpoint];
+        Vector2 targetgo = (target.position - transform.position).normalized;
 
-    Vector2 Nowmoving()
-    {
-        if (turnpoint)
+        transform.position += (Vector3)(targetgo * movespeed * Time.deltaTime);
+
+        float targetgogo = Vector2.Distance(transform.position, target.position);
+        if(targetgogo < 0.1f)
         {
-            return new Vector2(down.y, -down.x);
+            nowpoint = (nowpoint + 1) % turnpoint.Length;
         }
-        {
-            return new Vector2(-down.y, down.x);
-        }
+
+        rollthesurface(targetgo);
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void rollthesurface(Vector2 targetgo)
     {
-        if(collision.gameObject.layer == LayerMask.NameToLayer("turnpoint") && collision != lastturn)
-        {
-
-            lastturn = collision;
-
-            if (turnpoint)
-            {
-                down = new Vector2(down.y, -down.x);
-            }
-            else
-            {
-                down = new Vector2(-down.y, down.x);
-            }
-        }
+        float anchor = Mathf.Atan2(targetgo.y, targetgo.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, anchor + 90f);
     }
 
-    void rollthesurface()
-    {
-        transform.rotation = Quaternion.FromToRotation(Vector2.down, down);
-    }
 }
 
 
